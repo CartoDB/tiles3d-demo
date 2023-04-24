dec8d14cbf29d69bc8a0eded4130db3b6b0c52eb
Felix Palmer <felixpalmer@gmail.com>
2023-04-24 11:06:37 +0200

    Speed up data requests via local proxy

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,62 @@
 import svgr from 'vite-plugin-svgr'
 import zlib from 'zlib';
 
+// Rewrite URLs in instantiation & tilejson response to use our endpoint
+const writeBody = (buffer, res) => {
+  let remoteBody = buffer.toString();
+  const isMapInstantiation = remoteBody.includes('nrows');
+  const endpoint = isMapInstantiation ? 'carto-api' : 'carto-data-api';
+  remoteBody = remoteBody.replace('https://gcp-us-east1.api.carto.com/', `http://127.0.0.1:8080/${endpoint}/`); // Point at non-selfHandleResponse endpoint
+  res.write(remoteBody);
+  res.end();
+}
+
 export default {
   plugins: [svgr()],
   server: {
     host: '127.0.0.1',
     port: 8080,
     proxy: {
-      '/cartoapi': {
+      '/carto-api': {
         target: 'https://gcp-us-east1.api.carto.com',
         selfHandleResponse : true,
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/cartoapi/, ''),
+        rewrite: path => path.replace(/^\/carto-api/, ''),
         configure: proxy => {
           proxy.on('proxyRes', (proxyRes, req, res) => {
             proxyRes.headers['cache-control'] = 'public,max-age=100000,must-revalidate';
-            // Modify response
+
+            // Modify response for tilejson to update URLs
             let body = [];
+
             proxyRes.on('data', function (chunk) {
               body.push(chunk);
             });
             proxyRes.on('end', function () {
               body = Buffer.concat(body);
-              zlib.unzip(body, (err, buffer) => {
-                if (!err) {
-                  let remoteBody = buffer.toString();
-                  // remoteBody = remoteBody.replace() // do some string manipulation on remoteBody
-                  console.log(remoteBody);
-                  res.write(remoteBody);
-                  res.end();
-                } else {
-                  console.error(err);
-                }
-              });
+
+              if (proxyRes.headers['content-encoding']) {
+                zlib.unzip(body, (err, buffer) => {
+                  if (!err) {
+                    writeBody(buffer, res);
+                  } else {
+                    console.error(err);
+                  }
+                });
+              } else {
+                writeBody(body, res);
+              }
             });
+          });
+        }
+      },
+      '/carto-data-api': {
+        target: 'https://gcp-us-east1.api.carto.com',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/carto-data-api/, ''),
+        configure: proxy => {
+          proxy.on('proxyRes', (proxyRes, req, res) => {
+            proxyRes.headers['cache-control'] = 'public,max-age=100000,must-revalidate';
           });
         }
       },
