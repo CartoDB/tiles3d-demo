721b2c60642149f8a951a10a0a2e61876e80bcb3
Felix Palmer <felixpalmer@gmail.com>
2023-04-26 12:54:53 +0200

    Ditch deferred layer

--- layers/temperature.js
+++ layers/temperature.js
@@ -1,30 +1,23 @@
 import {CartoLayer, colorBins, MAP_TYPES} from '@deck.gl/carto';
-import DeferredLoadLayer from './deferredLoadLayer';
 import {_TerrainExtension as TerrainExtension} from '@deck.gl/extensions';
 import {colorToRGBArray} from '../utils';
 import {TEMPERATURE_COLOR_SCALE} from './colorScales';
 
 const {colors, labels} = TEMPERATURE_COLOR_SCALE;
 
-const _TemperatureLayer = DeferredLoadLayer(() => {
-  return new CartoLayer({
-    id: 'temperature',
-    connection: 'bigquery',
-    type: MAP_TYPES.RASTER,
-    data: 'cartobq.public_account.temperature_raster_int8',
-    formatTiles: 'binary',
-    tileSize: 256,
-    getFillColor: colorBins({
-      attr: 'band_1',
-      domain: labels,
-      colors: colors.map(colorToRGBArray)
-    }),
-    opacity: 0.5,
-
-    extensions: [new TerrainExtension()],
-  });
-});
-
-export const TemperatureLayer = new _TemperatureLayer({
+export const TemperatureLayer = new CartoLayer({
   id: 'temperature',
+  connection: 'bigquery',
+  type: MAP_TYPES.RASTER,
+  data: 'cartobq.public_account.temperature_raster_int8',
+  formatTiles: 'binary',
+  tileSize: 256,
+  getFillColor: colorBins({
+    attr: 'band_1',
+    domain: labels,
+    colors: colors.map(colorToRGBArray)
+  }),
+  opacity: 0.5,
+
+  extensions: [new TerrainExtension()],
 });
