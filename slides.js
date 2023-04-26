3556007ee6e27341ee8b843de529e827df7bf938
Alberto Asuero <albertoasuero@gmail.com>
2023-04-26 17:30:02 +0200

    Increase zoom

--- slides.js
+++ slides.js
@@ -18,17 +18,17 @@ export default [
   },
   /* 3 */ {
     layers: ['google-3d', 'parks'],
-    view: {latitude: 50.106134139314676, longitude: 14.418551788315758, bearing: -1.5982979378544293, pitch: 46.98403655241639, zoom: 14.157034959911423, height: 221.02488150953326}
+    view: {latitude: 50.106134139314676, longitude: 14.418551788315758, bearing: -1.5982979378544293, pitch: 46.98403655241639, zoom: 16.157034959911423, height: 221.02488150953326}
   },
   /* 4 */ {
     layers: ['google-3d', 'ft3t0pi'], // Top priority Buildings
-    view: {latitude: 50.098638970057976, longitude: 14.430949347805848, bearing: -58.668795330078694, pitch: 58.268384344242605, zoom: 15.751310628564266, height: 210},
+    view: {latitude: 50.098638970057976, longitude: 14.430949347805848, bearing: -58.668795330078694, pitch: 58.268384344242605, zoom: 17.751310628564266, height: 210},
     legend: {title: 'Distance', ...DISTANCE_COLOR_SCALE},
     orbit: true
   },
   /* 5 */ {
     layers: ['google-3d', 'wlixswr'], // Tree Planting Priority
-    view: {latitude: 50.09921060795221, longitude: 14.42477053670906, bearing: 41.67155588940855, pitch: 57.83916048618412, zoom: 15.388301111270481, height: 242.7711712},
+    view: {latitude: 50.09921060795221, longitude: 14.42477053670906, bearing: 41.67155588940855, pitch: 57.83916048618412, zoom: 17.388301111270481, height: 242.7711712},
     legend: {title: 'Priority', ...PRIORITY_COLOR_SCALE},
   }
 ];
