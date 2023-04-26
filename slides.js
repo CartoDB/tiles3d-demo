a6fc297939d418ab3780f1d4daa818ccd3350fb2
Felix Palmer <felixpalmer@gmail.com>
2023-04-26 11:14:05 +0200

    Update layers

--- slides.js
+++ slides.js
@@ -11,15 +11,15 @@ export default [
   },
   /* 2 */ {
     layers: ['google-3d', 'temperature'],
-    view: {latitude: 50.094310100945165, longitude: 14.428403524611161, bearing: 7.919687982872942, pitch: 27.029786864591166, zoom: 12.113912734993061, height: 221.10726674564285}
+    view: {latitude: 50.10367638816013, longitude: 14.433798010872685, bearing: -43.50888344569848, pitch: 47.70381729182906, zoom: 14.066421513772516, height: 221.10726674564285}
   },
   /* 3 */ {
-    layers: ['google-3d', 'mijq2d'], // Top priority Buildings
+    layers: ['google-3d', '6ohyvu4'], // Top priority Buildings
     view: {latitude: 50.098638970057976, longitude: 14.430949347805848, bearing: -58.668795330078694, pitch: 58.268384344242605, zoom: 15.751310628564266, height: 210},
     orbit: true
   },
   /* 4 */ {
-    layers: ['google-3d', 'mtcqiik'], // Tree Planting Priority
+    layers: ['google-3d', 'mijq2d'], // Tree Planting Priority
     view: {latitude: 50.08982364563372, longitude: 14.476247593416911, bearing: -53.76372618207482, pitch: 43.14705882352941, zoom: 12.601748373210508, height: 250}
   }
 ];
