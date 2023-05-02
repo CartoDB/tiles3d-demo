af6ff7a7e3ea4c077ca9e7ec7611be3b8021b470
Felix Palmer <felixpalmer@gmail.com>
2023-05-02 18:23:16 +0200

    Celsius

--- layers/colorScales.js
+++ layers/colorScales.js
@@ -1,5 +1,5 @@
 export const TEMPERATURE_COLOR_SCALE = {
-  labels: [26, 28, 30, 32, 34, 36],
+  labels: [26, 28, 30, 32, 34, 36].map(n => `${n} °C`),
   values: [20, 28, 30, 32, 34, 36],
   colors: ['#0d0887', '#6a00a8', '#b12a90', '#e16462', '#fca636', '#f0f921']
 }
