8c00cb1dc87470af2d35f52ddb81eac1c376ea0b
Felix Palmer <felixpalmer@gmail.com>
2023-05-15 15:10:52 +0200

    Use new map data

--- layers/remote.js
+++ layers/remote.js
@@ -2,8 +2,8 @@ import {fetchMap} from '@deck.gl/carto';
 import {DataFilterExtension, _TerrainExtension as TerrainExtension} from '@deck.gl/extensions';
 import {FADE_IN_COLOR} from './transitions';
 
-const cartoMapId = '60f339dd-450b-4c54-a402-41eb2d7a06af';
-// const cartoMapId = '87ded938-6337-45b6-8de1-4c2813e3d9c6';
+// const cartoMapId = '60f339dd-450b-4c54-a402-41eb2d7a06af';
+const cartoMapId = '87ded938-6337-45b6-8de1-4c2813e3d9c6';
 export async function fetchRemoteLayers() {
   const {layers} = await fetchMap({cartoMapId});
   if(location.host.includes('127.0.0.1')) {
@@ -15,7 +15,7 @@ export async function fetchRemoteLayers() {
       extensions: [],
       transitions: FADE_IN_COLOR
     }
-    if (['ft3t0pi', 'wlixswr'].includes(l.id)) {
+    if (['ft3t0pi', 'lvm6ojq'].includes(l.id)) {
       props.extensions.push(new DataFilterExtension({filterSize: 1}));
       if (l.id === 'ft3t0pi') {
         props.getFilterValue = f => f.properties.distance_to_nearest_tree;
@@ -24,34 +24,38 @@ export async function fetchRemoteLayers() {
       }
       props.filterRange = [0, 10000];
 
-      props.autoHighlight = true;
-      props.pickable = true;
-      props.extruded = true;
+      // Add autohighlight for tooltip
+      if (l.id === 'lvm6ojq') {
+        props.autoHighlight = true;
+        props.pickable = true;
+        props.extruded = true;
 
-      props.elevationScale = 1;
-      props.uniqueIdProperty = 'id';
+        props.elevationScale = 1;
+        props.uniqueIdProperty = 'id';
 
-      // Adjust data to have 3D offset. This doesn't work with picking
-      // for some reason. So instead keep zOffset as 0 but increase elevation
-      // to 300
-      const zOffset = 0;
-      props.getElevation = 300;
-      props.dataTransform = d => {
-        d.polygons.properties.forEach((p, i) => p.id = i);
-     
-        const {positions} = d.polygons;
-        const {value, size} = positions;
-        const n = value.length / size;
-        const newValue = new Float32Array(3 * n);
-        for (let i = 0; i < n; i++) {
-          newValue[3 * i + 0] = value[size * i + 0];
-          newValue[3 * i + 1] = value[size * i + 1];
-          newValue[3 * i + 2] = zOffset;
-        }
+        // Uncomment to see geometry under 3D tiles
+        // props.parameters = {depthTest: false};
 
-        positions.value = newValue;
-        positions.size = 3;
-        return d;
+        // Adjust data to have 3D offset. This doesn't work with picking
+        // for some reason.
+        // const zOffset = 200;
+        props.getElevation = 40; // Estimate for building height
+        props.dataTransform = d => {
+          const zOffset = d.polygons.numericProps.height.value;
+          const {positions} = d.polygons;
+          const {value, size} = positions;
+          const n = value.length / size;
+          const newValue = new Float32Array(3 * n);
+          for (let i = 0; i < n; i++) {
+            newValue[3 * i + 0] = value[size * i + 0];
+            newValue[3 * i + 1] = value[size * i + 1];
+            newValue[3 * i + 2] = zOffset[i] < 100 ? 250 : zOffset[i]; // Work around broken data
+          }
+
+          positions.value = newValue;
+          positions.size = 3;
+          return d;
+       }
       }
     }
     return l.clone(props);
