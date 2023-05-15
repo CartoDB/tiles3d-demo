e850df6869f5968a4ad0dd3cad2045568db4b0a9
Felix Palmer <felixpalmer@gmail.com>
2023-05-15 14:47:21 +0200

    PoC showing tooltip

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import {makeStyles, useMediaQuery} from '@material-ui/core';
+import {makeStyles, useMediaQuery, useTheme} from '@material-ui/core';
 import {DeckGL} from '@deck.gl/react';
 
 import {useAppState} from '../../state';
 import {SIDEBAR_WIDTH} from '../Sidebar/Sidebar';
 import CoverLogo from '../Cover/CoverLogo';
 import CoverAttribution from '../Cover/CoverAttribution';
+
+const SCORES = ['Very Low', 'Low', 'Medium', 'High', 'Very High'];
+
+const LABELS = [
+  'This group has a moderate level of urbanity and is characterized by a relatively small population of children under 15 years old, a small population of individuals over 65 years old, and a greater distance to the nearest area of dense vegetation. The group has a moderate number of buildings with an average building area. The land surface temperature is slightly lower than the total mean. The nitrogen dioxide concentration is significantly lower than the total mean. The group has a high priority for planting trees based on the Tree Planting Priority Score.',
+  'This group has a high level of urbanization, with a majority of the population being middle-aged or older. There is a low percentage of children under the age of 15. The group is located far from areas with dense vegetation and is surrounded by buildings, many of which are comparatively small. The average building size is also relatively small. The temperature and nitrogen dioxide concentration are moderate. The group has a moderately high priority for planting trees, indicating that trees could provide significant benefits for the residents.',
+  'This group has a relatively high urbanity level, with a large proportion of the population being above the age of 65 and a slightly below average population of children under the age of 15. The distance to the nearest area of dense vegetation is lower than the overall population mean, indicating that this area may benefit from more trees. The number of buildings is lower than the total mean, while the average buildings area is also lower than the total mean, suggesting a more compact living arrangement. Land surface temperature and nitrogen dioxide concentration are somewhat lower than the total mean. The Tree Planting Priority Score for this group is higher than the overall population mean, indicating a higher priority for planting trees.',
+  'This group has a high urbanity level and is located in a dense urban area. The population of this area has a significant number of young and elderly individuals, indicating a need for sustainable and healthy planning. The group has a moderate distance to the nearest area of dense vegetation, which could potentially be improved with tree planting. The number of buildings and their area are high, indicating an opportunity for green infrastructure. The Land Surface Temperature and the Nitrogen Dioxide concentration are slightly above the average, which may have negative impacts on the health and well-being of the population. The group has a high Tree Planting Priority Score, which suggests it should be a priority area for planting trees.',
+  'This group has a high level of urbanity and a large population with significant proportions of individuals both under 15 and over 65. The average distance to the nearest tree area is relatively high, suggesting that there may be a lack of green spaces in the area. The number and size of buildings in the area are also relatively high. The Land Surface Temperature and Nitrogen Dioxide concentration values are also higher than average. However, the group has a high priority for planting trees, as reflected by its high Tree Planting Priority Score. This is likely due to its high exposure score, which is a reflection of the potential benefits that tree planting could provide to the urban environment and the vulnerability of its population.'
+];
 
 const useStyles = makeStyles((theme) => ({
   mapWrapper: {
@@ -71,6 +81,7 @@ const Map = () => {
   const classes = useStyles();
   const {currentSlide, layers, viewState} = useAppState();
   const isDesktop = useMediaQuery((theme) => theme.breakpoints.up('md'));
+  const theme = useTheme();
 
   // Retina rendering very expensive on mobile, so limit resolution
   const useDevicePixels = isNaN(useDevicePixelsOverride) ? (isDesktop ? true : 0.75) : useDevicePixelsOverride;
@@ -95,6 +106,15 @@ const Map = () => {
               return viewState;
             }}
             useDevicePixels={useDevicePixels}
+            getTooltip={({object}) => {
+              if (!object) return;
+              const {tpp} = object.properties;
+              const i = Math.floor(tpp * 5);
+              return {
+                html: `<h3>Tree Planting Priority: ${SCORES[i]}</h3><div>${LABELS[i]}</div>`,
+                style: {borderRadius: '10px', backgroundColor: theme.palette.common.black, fontSize: '0.8em', width: '400px'}
+              }
+            }}
           >
           </DeckGL>
         </div>
--- layers/remote.js
+++ layers/remote.js
@@ -3,6 +3,7 @@ import {DataFilterExtension, _TerrainExtension as TerrainExtension} from '@deck.
 import {FADE_IN_COLOR} from './transitions';
 
 const cartoMapId = '60f339dd-450b-4c54-a402-41eb2d7a06af';
+// const cartoMapId = '87ded938-6337-45b6-8de1-4c2813e3d9c6';
 export async function fetchRemoteLayers() {
   const {layers} = await fetchMap({cartoMapId});
   if(location.host.includes('127.0.0.1')) {
@@ -10,7 +11,8 @@ export async function fetchRemoteLayers() {
   }
   return layers.map(l => {
     const props = {
-      extensions: [new TerrainExtension()],
+      //extensions: [new TerrainExtension()],
+      extensions: [],
       transitions: FADE_IN_COLOR
     }
     if (['ft3t0pi', 'wlixswr'].includes(l.id)) {
@@ -21,6 +23,36 @@ export async function fetchRemoteLayers() {
         props.getFilterValue = f => f.properties.tpp;
       }
       props.filterRange = [0, 10000];
+
+      props.autoHighlight = true;
+      props.pickable = true;
+      props.extruded = true;
+
+      props.elevationScale = 1;
+      props.uniqueIdProperty = 'id';
+
+      // Adjust data to have 3D offset. This doesn't work with picking
+      // for some reason. So instead keep zOffset as 0 but increase elevation
+      // to 300
+      const zOffset = 0;
+      props.getElevation = 300;
+      props.dataTransform = d => {
+        d.polygons.properties.forEach((p, i) => p.id = i);
+     
+        const {positions} = d.polygons;
+        const {value, size} = positions;
+        const n = value.length / size;
+        const newValue = new Float32Array(3 * n);
+        for (let i = 0; i < n; i++) {
+          newValue[3 * i + 0] = value[size * i + 0];
+          newValue[3 * i + 1] = value[size * i + 1];
+          newValue[3 * i + 2] = zOffset;
+        }
+
+        positions.value = newValue;
+        positions.size = 3;
+        return d;
+      }
     }
     return l.clone(props);
   });
